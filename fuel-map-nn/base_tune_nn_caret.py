65b08deafc4a3e1eba97bd85ed3fdc28f9a2fe50
Armando Castillo III <arcastilloiii@gmail.com>
2021-12-15 08:29:40 -0600

    base_tune_nn_caret
    base_tune_nn_caret: created conversion functions for afr_diff and 
    afr_lambda_to_afr. created function for instantiating the model. add 
    afr_diff column to the dataset.

--- fuel-map-nn/base_tune_nn_caret.py
+++ fuel-map-nn/base_tune_nn_caret.py
@@ -2,10 +2,34 @@ import pandas as pd
 import numpy as np
 from pycaret.regression import *
 
+def afr_lambda_to_afr(val):
+    return val*14.7
 
+def afr_diff(val, target_afr):
+    return 14-val
+
+def network(dataset, target, regression_algorithm):
+    setup(df, target, session_id=100)
+    model = create_model(regression_algorithm)
+    return model
+
+features = ['rpm','%','lambda']
 log_path='evo5-datalogs/log1.csv'
+headers = ["Sec", "km", "#", "rpm", "%", "lambda"]
+regression_algorithms = 'br'
+target = 'lambda'
+target_afr = 15.0
 
 df=pd.read_csv(log_path, skiprows=14, header=None)
-df=df.drop(labels=range(1,2), axis=0)
+df=df.drop(labels=range(0,2), axis=0)
 df=df.drop(columns=df.columns[6])
+df.columns = headers
+df['afr_diff'] = None
+df['lambda'] = df['lambda'].astype(float)
+for ind in df.index:
+    df['afr_diff'][ind] = afr_diff(afr_lambda_to_afr(df['lambda'][ind]),target_afr)
 df
+#create a loop to add afr_diff column
+
+net = network(df, target, regression_algorithms)
+# predictions
