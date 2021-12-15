3c84159e7f94ad473d6366c9f355785c39726dfa
Armando Castillo III <arcastilloiii@gmail.com>
2021-12-14 18:48:41 -0600

    evo5-datalogs; base_tune_nn_caret
    evo5-datalogs: moved dir into fuel-map-nn dir to allow for relative dir 
    call.
    
    base_tune_nn_caret: created dataframe for first set of logs.

--- fuel-map-nn/base_tune_nn_caret.py
+++ fuel-map-nn/base_tune_nn_caret.py
@@ -3,7 +3,9 @@ import numpy as np
 from pycaret.regression import *
 
 
-#log_path=evo5-datalogs/log1.csv
+log_path='evo5-datalogs/log1.csv'
 
-def log_dataframe(log_pathg):
-    pass
+df=pd.read_csv(log_path, skiprows=14, header=None)
+df=df.drop(labels=range(1,2), axis=0)
+df=df.drop(columns=df.columns[6])
+df
